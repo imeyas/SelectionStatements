3f335c9b26a6f0e50630dd07fa83e4d03d245e29
Ebo Onwunali <ebo@njgeneral.com>
2022-01-12 22:20:13 -0500

    First commit on If-Else and Switch Case Statements

---
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.Abs(10);
+            
+            
+            //If-Else Statements part of exercise
+
+            Console.WriteLine("What is my favorite number?");
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("SAME!");
+            }
+
+        }
+    }
+}
---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            var faveSubject = Console.ReadLine();
+            var answer = faveSubject;
+            switch(answer.ToLowerInvariant())
+            {
+                case "physical education":
+                case "gym":
+                case "pe":
+                    Console.WriteLine($"{answer} it is: Your health is your wealth!");
+                    break;
+
+                case "science":
+                case "biology":
+                case "chemistry":
+                case "lab":
+                case "bio":
+                case "chem":
+                    Console.WriteLine($"{answer} it is: Stay true to the scientific method, scientist...");
+                    break;
+
+                case "mathematic":
+                case "math":
+                case "algebra":
+                case "geometry":
+                case "calculus":
+                    Console.WriteLine($"{answer} it is: Get your Einstein on :-)");
+                    break;
+
+                case "psychology":
+                case "psych":
+                    Console.WriteLine($"{answer} it is: Shout-out Mr. Freud!");
+                    break;
+
+                case "music":
+                case "band":
+                case "music class":
+                    Console.WriteLine($"{answer} it is: Let us hear something fresh right quick, LOL!");
+                    break;
+
+                default:
+                    Console.WriteLine("Input not available, try again or click 'x' to exit...");
+                    break;
+
+                    
+            }
+
+
+        }
+    }
+}
